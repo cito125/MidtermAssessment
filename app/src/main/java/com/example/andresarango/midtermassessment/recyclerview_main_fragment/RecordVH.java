3f322334ca9de75b67ec8135204379cd2d742b9d
cito125 <cito125@gmail.com>
2016-12-10 17:41:42 -0500

    made background proper color

--- app/src/main/java/com/example/andresarango/midtermassessment/recyclerview_main_fragment/RecordVH.java
+++ app/src/main/java/com/example/andresarango/midtermassessment/recyclerview_main_fragment/RecordVH.java
@@ -1,6 +1,8 @@
 package com.example.andresarango.midtermassessment.recyclerview_main_fragment;
 
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -22,14 +24,15 @@ public class RecordVH extends RecyclerView.ViewHolder {
     }
 
     public void bind(com.example.andresarango.midtermassessment.modello.Record record) {
-
-//        int backgroundColor = record.getProfileBackground();
-        String userName = record.getUsername()
-                ;
+        if(record.getProfileBackground() != null){
+            Log.d("YEEEEERRR", record.getProfileBackground().substring(2));
+            int backgroundColor = Color.parseColor("#"+record.getProfileBackground().substring(2));
+            itemView.setBackgroundColor(backgroundColor);
+        }
+        String userName = record.getUsername();
         String likes = Integer.toString(record.getLiked());
         mLikedTV.setText(likes);
         mUsernameTV.setText(userName);
-//        itemView.setBackgroundColor(backgroundColor);
 
 
     }
